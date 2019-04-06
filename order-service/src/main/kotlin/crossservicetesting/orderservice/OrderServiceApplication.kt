b976bfe87633b7c8439719a65da0867eb0bde285
gilligan128 <gilligan128@gmail.com>
2019-04-06 11:29:56 -0400

    added rabbitmq binder to order service. playing around with cloud stream publishing

--- order-service/src/main/kotlin/crossservicetesting/orderservice/OrderServiceApplication.kt
+++ order-service/src/main/kotlin/crossservicetesting/orderservice/OrderServiceApplication.kt
@@ -1,10 +1,33 @@
 package crossservicetesting.orderservice
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.stream.annotation.StreamListener
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.cloud.stream.messaging.Source
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+import org.springframework.integration.support.MessageBuilder
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
-class OrderServiceApplication
+class OrderServiceApplication {
+
+	@Autowired
+	val source: Source? = null
+
+	@PostMapping("/order")
+	@SendTo(Source.OUTPUT)
+	@ResponseBody
+	fun postOrder(): String {
+		return "order 1"
+		//source.output().send(MessageBuilder.withPayload("order 1").build())
+	}
+}
+
 
 fun main(args: Array<String>) {
 	runApplication<OrderServiceApplication>(*args)
